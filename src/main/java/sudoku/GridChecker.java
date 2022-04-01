86dd63aa000a19f6ce0c79196e8f00f0d56a6389
Alexander Staeding <alexanderstaeding@gmail.com>
2022-04-01 14:32:15 +0200

    Add checkCatching to GridChecker

--- src/main/java/sudoku/GridChecker.java
+++ src/main/java/sudoku/GridChecker.java
@@ -3,4 +3,13 @@ package sudoku;
 public interface GridChecker {
 
     void check(int[][] grid);
+
+    default boolean checkCatching(int[][] grid) {
+        try {
+            check(grid);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
